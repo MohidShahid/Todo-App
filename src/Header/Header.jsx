594734d4837ce0a6bb42ed9b25633890f1b42fd5
MohidShahid <mohid.fantasy@gmail.com>
2024-09-21 07:04:12 +0500

    Vite was outdated , Header Link wasn't working
    i install npm i vite --save-dev and wrap react -router-dom LINK on Flowbite Navbar.Link

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,24 +5,41 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <Navbar fluid rounded>
-    <Navbar.Brand >
-      
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-    </Navbar.Brand>
-    <div className="flex md:order-2">
-      <Button>Get started</Button>
-      <Navbar.Toggle />
-    </div>
-    <Navbar.Collapse>
-      <Navbar.Link href="#" active>
-        Home
-      </Navbar.Link>
-      <Navbar.Link Link={"/about"}>About</Navbar.Link>
-      <Navbar.Link Link={"/services"}>Services</Navbar.Link>
-      <Navbar.Link Link={"/todo-app"}>Todo App</Navbar.Link>
-      <Navbar.Link Link={"/contact"}>Contact</Navbar.Link>
-    </Navbar.Collapse>
-  </Navbar>
+      <Navbar.Brand>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Link to="/">
+          <Navbar.Link href="/" active>
+            Home
+          </Navbar.Link>
+        </Link>
+        <Link to="/about">
+          <Navbar.Link href="/about">
+            About
+          </Navbar.Link>
+        </Link>
+        <Link to="/services">
+          <Navbar.Link href="/services">
+            Services
+          </Navbar.Link>
+        </Link>
+        <Link to="/todo-app">
+          <Navbar.Link href="/todo-app">
+            Todo App
+          </Navbar.Link>
+        </Link>
+        <Link to="/contact">
+          <Navbar.Link href="/contact">
+            Contact
+          </Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
